228505b0032c12b692c2cbce9481162e00a9d6cd
hya0906 <59861622+hya0906@users.noreply.github.com>
2022-11-28 13:53:45 +0900

    Add files via upload

---
+++ study/R문제/2_.R
@@ -0,0 +1,11 @@
+getwd()
+setwd('D://downloads/R_resource')
+getwd()
+
+library(dplyr) #라이브러리 장착
+library(ggplot2)
+
+str(cars) #데이터의 구조를 보여주는 함수 str
+head(cars) #6개의 데이터와 라벨을 보여주는 함수
+plot(Cars) #데이터 및 대상의 그래프를 그려주는 함수
+cars
